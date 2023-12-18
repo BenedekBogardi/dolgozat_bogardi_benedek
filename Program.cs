70b0db7c8622c05e77cdb2831944f7e91574eca6
BenedekBogardi <153838403+BenedekBogardi@users.noreply.github.com>
2023-12-18 11:58:05 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bogardi_gitt
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
